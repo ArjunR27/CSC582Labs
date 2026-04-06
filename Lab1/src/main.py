904d53f2394c976c28f56f63f051a22ff5c4d274
Sid Meka <sidmekarao@gmail.com>
2026-04-06 11:35:06 -0700

    comine

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -15,8 +15,10 @@ def main():
     credits_df = credits_to_df()
     movies_df = movies_to_df()
 
-    combined_df = movies_df.merge(credits_df, left_on='id', right_on='movie_id', how='inner')
-    combined_df.to_csv("../movie_data/combined.csv")
+    #combined_df = movies_df.merge(credits_df, left_on='id', right_on='movie_id', how='inner')
+    #combined_df.to_csv("../movie_data/combined.csv")
+    combined_df = pd.read_csv("../movie_data/tmdb_5000_movies.csv")
+
     print(combined_df.head())
 
 
